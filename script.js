6932a06e96f43bc0a2000f28ab037536f6f620f2
Satindra Ramalingam <75828216+satindra-r@users.noreply.github.com>
2024-11-23 21:59:20 +0530

    added another button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const button = document.getElementById("B1");
-button.addEventListener("click", function() {
-    button.innerHTML = 1-button.innerHTML;
+const button1 = document.getElementById("B1");
+const button2 = document.getElementById("B2");
+button1.addEventListener("click", function() {
+    button2.innerHTML = 1-button2.innerHTML;
+});
+button2.addEventListener("click", function() {
+    button1.innerHTML = 1-button1.innerHTML;
 });
